a133c9ff21607ab6ac8863f7cd4dc64b37ead70c
Yinan Liu <liu.damian@gmail.com>
2017-06-27 21:44:12 +0200

    remove web.xml

--- configuration-client/src/main/java/hello/ConfigClientApplication.java
+++ configuration-client/src/main/java/hello/ConfigClientApplication.java
@@ -3,19 +3,31 @@ package hello;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.context.refresh.ContextRefresher;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-
+@Configuration
+@ComponentScan(basePackages = "hello")
+@EnableAutoConfiguration
 @PropertySource(value = "classpath:application.properties")
 public class ConfigClientApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ConfigClientApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class, args);
--- configuration-service/src/main/java/hello/ConfigServiceApplication.java
+++ configuration-service/src/main/java/hello/ConfigServiceApplication.java
@@ -1,16 +1,26 @@
 package hello;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @EnableConfigServer
 @SpringBootApplication
 @PropertySource(value = "classpath:application.properties")
+@Configuration
+@EnableAutoConfiguration
 public class ConfigServiceApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ConfigServiceApplication.class);
+    }
     
     public static void main(String[] args) {
         SpringApplication.run(ConfigServiceApplication.class, args);
